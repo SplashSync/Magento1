fd0bfef175bd56fc6c35ec618401019fb3f57560
BadPixxel <eshop.bpaquier@gmail.com>
2018-04-09 19:24:48 +0200

    WIP : Add Travis CI

--- build/configure.sh
+++ build/configure.sh
@@ -10,30 +10,28 @@ echo "Enable Splash Module"
 echo "Configure Store Main Options"
 
 ./n98-magerun.phar config:set general/store_information/name                      "Magento 1" 
-#./n98-magerun.phar --root-dir=htdocs --scope-id=0 config:set general/store_information/name                      "Magento 1" 
-./n98-magerun.phar --root-dir=htdocs config:set general/store_information/address                   "Store Address"
-./n98-magerun.phar --root-dir=htdocs config:set general/store_information/merchant_country          "France"
-# ./n98-magerun.phar --root-dir=htdocs config:set web/secure/base_url                               "www.splashsync.com"
-# ./n98-magerun.phar --root-dir=htdocs config:set trans_email/ident_general/email                   "contact@store"
-./n98-magerun.phar --root-dir=htdocs config:set general/store_information/phone                     "0123456789"
+./n98-magerun.phar config:set general/store_information/address                   "Store Address"
+./n98-magerun.phar config:set general/store_information/merchant_country          "France"
+./n98-magerun.phar config:set general/store_information/phone                     "0123456789"
 
-./n98-magerun.phar --root-dir=htdocs config:get general/store_information/* 
-
-./n98-magerun.phar --root-dir=htdocs cache:clean
-./n98-magerun.phar cache:clean
+./n98-magerun.phar config:get general/store_information/* 
 
 echo "Configure Splash Module Options"
 
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/advanced/expert             0 
+./n98-magerun.phar config:set splashsync_splash_options/advanced/expert             0 
 
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/core/id                     DoNotUseThisId 
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/core/key                    DoNotUseThisKey 
+./n98-magerun.phar config:set splashsync_splash_options/core/id                     DoNotUseThisId 
+./n98-magerun.phar config:set splashsync_splash_options/core/key                    DoNotUseThisKey 
     
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/user/login                  admin 
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/user/pwd                    password123 
+./n98-magerun.phar config:set splashsync_splash_options/user/login                  admin 
+./n98-magerun.phar config:set splashsync_splash_options/user/pwd                    password123 
 
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/products/attribute_set      4 
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/products/default_stock      1 
-./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/thirdparty/store            1 
+./n98-magerun.phar config:set splashsync_splash_options/products/attribute_set      4 
+./n98-magerun.phar config:set splashsync_splash_options/products/default_stock      1 
+./n98-magerun.phar config:set splashsync_splash_options/thirdparty/store            1 
 
-./n98-magerun.phar --root-dir=htdocs config:get splashsync_splash_options/* 
+./n98-magerun.phar config:get splashsync_splash_options/* 
+
+echo "Clean Magento Cache"
+
+./n98-magerun.phar cache:clean
