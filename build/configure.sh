101d2759d8d7ef5efb01409260f873ffb985c1eb
BadPixxel <eshop.bpaquier@gmail.com>
2018-04-09 17:17:38 +0200

    WIP : Add Travis CI

--- build/configure.sh
+++ build/configure.sh
@@ -1,33 +1,33 @@
 cd /tmp/magetest
 
 echo "Insert Dummy data in Store"
-tools/n98-magerun.phar dev:module:disable SplashSync_Splash
-tools/n98-magerun.phar customer:create:dummy 5 en_US
+./n98-magerun.phar dev:module:disable SplashSync_Splash
+./n98-magerun.phar customer:create:dummy 5 en_US
 
 echo "Enable Splash Module"
-tools/n98-magerun.phar dev:module:enable SplashSync_Splash
+./n98-magerun.phar dev:module:enable SplashSync_Splash
 
 echo "Configure Store Main Options"
 
-tools/n98-magerun.phar --root-dir=htdocs config:set general/store_information/name                      "Magento 1" 
-tools/n98-magerun.phar --root-dir=htdocs config:set general/store_information/address                   "Store Address"
-tools/n98-magerun.phar --root-dir=htdocs config:set general/store_information/merchant_country          "France"
-# tools/n98-magerun.phar --root-dir=htdocs config:set web/secure/base_url                               "www.splashsync.com"
-# tools/n98-magerun.phar --root-dir=htdocs config:set trans_email/ident_general/email                   "contact@store"
-tools/n98-magerun.phar --root-dir=htdocs config:set general/store_information/phone                     "0123456789"
+./n98-magerun.phar --root-dir=htdocs config:set general/store_information/name                      "Magento 1" 
+./n98-magerun.phar --root-dir=htdocs config:set general/store_information/address                   "Store Address"
+./n98-magerun.phar --root-dir=htdocs config:set general/store_information/merchant_country          "France"
+# ./n98-magerun.phar --root-dir=htdocs config:set web/secure/base_url                               "www.splashsync.com"
+# ./n98-magerun.phar --root-dir=htdocs config:set trans_email/ident_general/email                   "contact@store"
+./n98-magerun.phar --root-dir=htdocs config:set general/store_information/phone                     "0123456789"
 
 echo "Configure Splash Module Options"
 
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/advanced/expert             0 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/advanced/expert             0 
 
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/core/id                     DoNotUseThisId 
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/core/key                    DoNotUseThisKey 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/core/id                     DoNotUseThisId 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/core/key                    DoNotUseThisKey 
     
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/user/login                  admin 
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/user/pwd                    password123 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/user/login                  admin 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/user/pwd                    password123 
 
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/products/attribute_set      4 
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/products/default_stock      1 
-tools/n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/thirdparty/store            1 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/products/attribute_set      4 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/products/default_stock      1 
+./n98-magerun.phar --root-dir=htdocs config:set splashsync_splash_options/thirdparty/store            1 
 
-tools/n98-magerun.phar --root-dir=htdocs config:get splashsync_splash_options/* 
+./n98-magerun.phar --root-dir=htdocs config:get splashsync_splash_options/* 
